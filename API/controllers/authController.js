730fe3913de8844d74191bda9991f4aab789122d
Nilesh Shetty <nileshshetty45@gmail.com>
2024-08-28 23:29:45 +0530

    complete signup page ui

--- API/controllers/authController.js
+++ API/controllers/authController.js
@@ -1,6 +1,6 @@
 import User from "../models/userModel.js";
 import bcrypt from "bcryptjs";
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashPassword = bcrypt.hashSync(password, 11);
   const newUser = new User({ username, email, password: hashPassword });
@@ -8,6 +8,6 @@ export const signup = async (req, res) => {
     await newUser.save();
     res.status(201).json({ message: "user created successfully" });
   } catch (error) {
-    res.status(500).json(error.message);
+    next(error);
   }
 };
