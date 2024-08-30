81c1bdb13e3386b80658adfe97bb9450668e619f
Nilesh Shetty <nileshshetty45@gmail.com>
2024-08-30 11:56:31 +0530

    created signin API route, used jwtsign and cookie to store data.

--- API/controllers/authController.js
+++ API/controllers/authController.js
@@ -1,12 +1,33 @@
 import User from "../models/userModel.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { errorHandler } from "../utils/error.js";
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
-  const hashPassword = bcrypt.hashSync(password, 11);
-  const newUser = new User({ username, email, password: hashPassword });
+  const hashedPassword = bcrypt.hashSync(password, 11);
+  const newUser = new User({ username, email, password: hashedPassword });
   try {
     await newUser.save();
     res.status(201).json({ message: "user created successfully" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "user not found"));
+    const validPassword = bcrypt.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, "wrong credentials"));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: hashedPassword, ...rest } = validUser._doc;
+    const expiryDate = new Date(Date.now() + 3600000);
+    res
+      .cookie("access_token", token, { httpOnly: true, expires: expiryDate }) // access_token is the name of the variable inside which the data will be stored
+      .status(200)
+      .json(rest); // the data that we get from the cookies will be used for creating the UI of the page
   } catch (error) {
     next(error);
   }
