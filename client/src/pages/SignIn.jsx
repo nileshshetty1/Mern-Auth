b5becf77f0cc56dec6a55c70f81313fb21f5283c
nileshshetty1 <nileshshetty45@gmail.com>
2024-08-31 20:23:44 +0530

    added redux toolkit for signin page

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,18 +1,24 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import {
+  signInFailure,
+  signInStart,
+  signInSuccess,
+} from "../redux/user/userSlice.js";
+import { useDispatch, useSelector } from "react-redux";
 const SignIn = () => {
   const [formData, setFormData] = useState({});
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
+  const {loading, error}=useSelector(state => state.user)
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
   };
   const handleSubmit = async (e) => {
     e.preventDefault();
-    setLoading(true);
-    setError(false);
+
     try {
+      dispatch(signInStart())
       const res = await fetch("/api/auth/signin", {
         method: "POST",
         headers: {
@@ -21,18 +27,22 @@ const SignIn = () => {
         body: JSON.stringify(formData),
       });
       const data = await res.json();
-      console.log(data);
-      setLoading(false);
+      
       if (data.success === false) {
-        setError(true);
+        dispatch(signInFailure(data))
         return;
       }
+      dispatch(signInSuccess(data))
       navigate("/");
     } catch (error) {
-      setLoading(false);
-      setError(true);
+      dispatch(signInFailure(error))  
+      
     }
   };
+
+
+
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7 ">Sign In</h1>
@@ -61,9 +71,91 @@ const SignIn = () => {
           <span className="text-blue-500">Sign Up</span>
         </Link>
       </div>
-      <p className="text-red-700 mt-5">{error && "something went wrong"}</p>
+      <p className="text-red-700 mt-5">{error ? error.message || "something went wrong":""}</p>
     </div>
   );
 };
 
 export default SignIn;
+
+
+// import { useState } from 'react';
+// import { Link, useNavigate } from 'react-router-dom';
+// import {
+//   signInStart,
+//   signInSuccess,
+//   signInFailure,
+// } from '../redux/user/userSlice';
+// import { useDispatch, useSelector } from 'react-redux';
+
+
+// export default function SignIn() {
+//   const [formData, setFormData] = useState({});
+//   const { loading, error } = useSelector((state) => state.user);
+
+//   const navigate = useNavigate();
+//   const dispatch = useDispatch();
+//   const handleChange = (e) => {
+//     setFormData({ ...formData, [e.target.id]: e.target.value });
+//   };
+
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+//     try {
+//       dispatch(signInStart());
+//       const res = await fetch('/api/auth/signin', {
+//         method: 'POST',
+//         headers: {
+//           'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify(formData),
+//       });
+//       const data = await res.json();
+//       if (data.success === false) {
+//         dispatch(signInFailure(data));
+//         return;
+//       }
+//       dispatch(signInSuccess(data));
+//       navigate('/');
+//     } catch (error) {
+//       dispatch(signInFailure(error));
+//     }
+//   };
+//   return (
+//     <div className='p-3 max-w-lg mx-auto'>
+//       <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
+//       <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+//         <input
+//           type='email'
+//           placeholder='Email'
+//           id='email'
+//           className='bg-slate-100 p-3 rounded-lg'
+//           onChange={handleChange}
+//         />
+//         <input
+//           type='password'
+//           placeholder='Password'
+//           id='password'
+//           className='bg-slate-100 p-3 rounded-lg'
+//           onChange={handleChange}
+//         />
+//         <button
+//           disabled={loading}
+//           className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
+//         >
+//           {loading ? 'Loading...' : 'Sign In'}
+//         </button>
+      
+//       </form>
+//       <div className='flex gap-2 mt-5'>
+//         <p>Dont Have an account?</p>
+//         <Link to='/sign-up'>
+//           <span className='text-blue-500'>Sign up</span>
+//         </Link>
+//       </div>
+//       <p className='text-red-700 mt-5'>
+//         {error ? error.message || 'Something went wrong!' : ''}
+//       </p>
+//     </div>
+//   );
+// }
